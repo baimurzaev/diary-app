98783b8d75d63de7d49c092300a1f3040f968d76
Ruslan <baimurzaev@gmail.com>
2022-12-27 00:50:20 +0300

    Added migrations

--- database/migrations/2022_12_26_211631_create_schedule_lessons_tmpl.php
+++ database/migrations/2022_12_26_211631_create_schedule_lessons_tmpl.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateScheduleLessonsTable extends Migration
+class CreateScheduleLessonsTmpl extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,13 @@ class CreateScheduleLessonsTable extends Migration
      */
     public function up()
     {
-        Schema::create('schedule_lessons', function (Blueprint $table) {
+        Schema::create('schedule_lessons_tmpl', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('subject_id')->index();
-            $table->bigInteger('schedule_id')->index();
             $table->string('title', 192)->nullable();
-            $table->string('homework', 255)->nullable();
-            $table->dateTime('lesson_start');
-            $table->dateTime('lesson_end');
+            $table->bigInteger('schedule_id')->index();
+            $table->dateTime('time_start');
             $table->tinyInteger('day_num');
             $table->tinyInteger('position')->default(1);
-            //$table->foreignId('subject_id')->constrained('subjects');
             $table->timestamps();
         });
     }
@@ -35,6 +31,6 @@ class CreateScheduleLessonsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('schedule_lessons');
+        Schema::dropIfExists('schedule_lessons_tmpl');
     }
 }
--- database/migrations/2022_12_26_211659_create_schedules_tmpl.php
+++ database/migrations/2022_12_26_211659_create_schedules_tmpl.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSchedulesTable extends Migration
+class CreateSchedulesTmpl extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateSchedulesTable extends Migration
      */
     public function up()
     {
-        Schema::create('schedules', function (Blueprint $table) {
+        Schema::create('schedules_tmpl', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
             $table->bigInteger('user_id');
             $table->bigInteger('classroom_id');
             $table->date('time_start');
-            //$table->foreignId('user_id')->constrained('users');
-            //$table->foreignId('classroom_id')->constrained('classrooms');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateSchedulesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('schedules');
+        Schema::dropIfExists('schedules_tmpl');
     }
 }
